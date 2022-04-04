aaf291ccdf876d1f547b902083061512eb2567a8
Gleb Burylov <bimbur2014@gmail.com>
2022-04-04 13:11:17 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <validator />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+<script>
+import Validator from "@/views/validator";
+export default {
+  components: { Validator },
+};
+</script>
